2a0c7f723f3a1116dcbaef04df3a3f67742c5a64
Ofek-Hodis <ofkhod@gmail.com>
2025-08-24 23:44:24 +0300

    Functioning improved menus

--- main_loops.py
+++ main_loops.py
@@ -21,12 +21,13 @@ pygame.time.set_timer(SCREEN_UPDATE, 90)  # Setting a timer to trigger the event
 gameover_sound_played = False  # Defining a variable to prevent replay of gameover sound
 font = 'Fonts/Cute Dino.ttf'
 
+
 def menu_loop():
     global font  # Telling the function to use the global font variable
     while True:
         screen.fill((175, 215, 70))
 
-        title_position = (cell_number / 2 * cell_size), (cell_number / 8 * cell_size)  # Defining position for the text
+        title_position = (cell_number / 2 * cell_size), (3 * cell_size)  # Defining position for the text
         # Using a function to draw the title
         title_draw("snake", title_position, font, 50, (255, 255, 255), screen)
 
@@ -72,6 +73,9 @@ def menu_loop():
                 if start_button.check_input(pygame.mouse.get_pos()):
                     snake_loop()
                     break
+                elif two_player_button.check_input(pygame.mouse.get_pos()):
+                    twoplayer_loop()
+                    break
                 elif quit_button.check_input(pygame.mouse.get_pos()):
                     pygame.quit()  # Closing game
                     sys.exit()
@@ -89,7 +93,7 @@ def gameover_actions():
         gameover_sound_played = True
 
 
-def gamedone_loop():
+def gamedone_loop(score):
     main_game = Main()
     global gameover_sound_played  # Telling the function to use the global variable
     global font
@@ -97,9 +101,13 @@ def gamedone_loop():
     while True:#Score and best score
         screen.fill((20, 20, 20))
 
-        title_position = (cell_number/2 * cell_size), (cell_number/8 * cell_size)  # Defining position for the text
+        title_position = (cell_number/2 * cell_size), (3 * cell_size)  # Defining position for the text
         # Using a function to draw the title
         title_draw("Game Over", title_position, font, 50, (255, 255, 255), screen)
+
+        score_position = (cell_number / 2 * cell_size), (5 * cell_size)  # Defining position for the text
+        # Using a function to draw the score
+        title_draw("Final Score: " + str(score), score_position, font, 35, (255, 255, 255), screen)
 
         # Setting up restart button
         restart_position = (cell_number/2 * cell_size), (10 * cell_size)  # Positioning button
@@ -175,11 +183,50 @@ def snake_loop():
         screen.fill((175, 215, 70))  # Creating a tuple with rgb values (out of 255) to define the color of the screen
         main_game.draw_elements()
         if not main_game.game_active:  # Updating the screen
-            gamedone_loop()
+            gamedone_loop(len(main_game.snake.body) - 3)  # Calculating the final score
             break
 
         pygame.display.update()
         clock.tick(60)  # Limiting the loop (and the game) to 60 fps
+
+
+def twoplayer_loop():
+    main_game = Main()  # An object that will be used to follow the game and execute commands
+    while True:  # Infinite loop I will break when I want the game to stop
+        for event in pygame.event.get():  # When starting the game we check for all events
+            if event.type == pygame.QUIT:  # If the user closes the window, quit the program
+                pygame.quit()
+                sys.exit()  # Halting the code
+            if event.type == SCREEN_UPDATE:  # Moving the snake constantly when updating the screen
+                main_game.update()
+            if event.type == pygame.KEYDOWN:  # Checking for user input and defining direction by the key
+                if event.key == pygame.K_UP:
+                    if main_game.snake.direction.y != 1:  # Preventing direction change from up to down
+                        main_game.snake.next_direction = Vector2(0, -1)
+                if event.key == pygame.K_DOWN:
+                    if main_game.snake.direction.y != -1:  # Preventing direction change from down to up
+                        main_game.snake.next_direction = Vector2(0, 1)
+                if event.key == pygame.K_RIGHT:
+                    if main_game.snake.direction.x != -1:
+                        main_game.snake.next_direction = Vector2(1, 0)  # Preventing direction change from left to right
+                if event.key == pygame.K_LEFT:
+                    if main_game.snake.direction.x != 1:
+                        main_game.snake.next_direction = Vector2(-1, 0)  # Preventing direction change from right to left
+
+                if event.key == pygame.K_r:
+                    main_game = Main()  # Restarting the game if the input is r
+                elif event.key == pygame.K_ESCAPE:
+                    main_game.close_game()
+
+        screen.fill((175, 215, 70))  # Creating a tuple with rgb values (out of 255) to define the color of the screen
+        main_game.draw_elements()
+        if not main_game.game_active:  # Updating the screen
+            gamedone_loop(len(main_game.snake.body) - 3)  # Calculating the final score
+            break
+
+        pygame.display.update()
+        clock.tick(60)  # Limiting the loop (and the game) to 60 fps
+
 
 menu_loop()
 
